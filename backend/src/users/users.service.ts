8901ffc0e7cfbb7e44e44ea2a15c3089f254fc07
Noah Janzen <noah.janzen@hs-ruhrwest.de>
2022-06-08 12:08:29 +0200

    BE: Adds documentation to the users service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -10,16 +10,31 @@ import { CreateUserDto } from 'src/auth/dto';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
+  /**
+   * Creates a new user in the database
+   * @param user to be stored
+   * @returns the new user
+   */
   async create(user: CreateUserDto & { confirmationToken: string }) {
     const newUser = new this.userModel(user);
     await newUser.save();
     return newUser;
   }
 
+  /**
+   * Finds an user by his/her id
+   * @param userId of the user to find
+   * @returns the user
+   */
   async findById(userId: string) {
     return this.userModel.findById(userId).exec();
   }
 
+  /**
+   * Finds an user by his/her email
+   * @param email of the user to find
+   * @returns the user
+   */
   async findOne(email: string) {
     let user;
     try {
@@ -31,6 +46,10 @@ export class UsersService {
     return user;
   }
 
+  /**
+   * Confirms an user
+   * @param userId of the user to be confirmed
+   */
   async confirm(userId: string) {
     const user = await this.userModel.findById(userId).exec();
 
@@ -41,11 +60,20 @@ export class UsersService {
     await user.save();
   }
 
+  /**
+   * Stores the hashed refresh token to the passed user
+   * @param user to be updated
+   * @param hashedRefreshToken hash of the refresh token
+   */
   async updateRefreshToken(user: UserDocument, hashedRefreshToken: string) {
     user.refreshToken = hashedRefreshToken;
     await user.save();
   }
 
+  /**
+   * Deletes the refresh token from the passed user
+   * @param user to be updated
+   */
   async deleteRefreshToken(user: UserDocument) {
     user.refreshToken = null;
     await user.save();
