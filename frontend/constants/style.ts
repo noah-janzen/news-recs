7db6df7c068d9ad65ed62996f01d28153e6f68c8
Noah Janzen <noah.janzen@hs-ruhrwest.de>
2022-06-24 11:37:01 +0200

    FE: Color schema added

---
+++ frontend/constants/style.ts
@@ -0,0 +1,15 @@
+export const GlobalStyles = {
+  colors: {
+    primary100: '#5B8BB0',
+    primary200: '#386C95',
+    primary300: '#1E5987',
+    primary400: '#114369',
+    primary500: '#042C4A',
+
+    secondary100: '#FFC77B',
+    secondary200: '#E7A54D',
+    secondary300: '#D28723',
+    secondary400: '#A36511',
+    secondary500: '#734200',
+  },
+}
