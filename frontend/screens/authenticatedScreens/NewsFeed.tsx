268b9a515cf975aa7c514d7d2a289e65d7204cd3
Noah Janzen <noah.janzen@hs-ruhrwest.de>
2022-07-14 13:16:23 +0200

    FE: Avoids unneccessary backend calls to store interactions

--- frontend/screens/authenticatedScreens/NewsFeed.tsx
+++ frontend/screens/authenticatedScreens/NewsFeed.tsx
@@ -1,37 +1,31 @@
 import { useEffect, useRef, useState } from 'react'
 import { ActivityIndicator, FlatList, StyleSheet, View } from 'react-native'
-import { useSelector, useDispatch } from 'react-redux'
 
 import { NewsDto } from '../../model/dto/News.dto'
 import NewsItem from '../../components/ui/NewsItem/NewsItem'
 import { getNews } from '../../api/news'
 import { addInteraction as addInteractionAPI } from '../../api/interaction'
 import { addInteraction } from '../../store/interactionsSlice'
-import { StoreReducer } from '../../store/store'
+import { store } from '../../store/store'
 
 function NewsFeed() {
   const [isInitialLoading, setIsInitialLoading] = useState(true)
   const [isRefreshing, setIsRefreshing] = useState(false)
   const [newsItems, setNewsItems] = useState<NewsDto[]>([])
 
-  const dispatch = useDispatch()
-
-  const interactions = useSelector(
-    (state: StoreReducer) => state.interactions.interactions
-  )
-
-  const onViewRef = useRef((viewableItems) => {
+  const onViewRef = useRef((viewableItems: any) => {
     viewableItems.changed.forEach((visibleNewsArticle: any) => {
       const newsArticleId = visibleNewsArticle.item.id
+      const interactions = store.getState().interactions.interactions
 
-      // Store interaction in backend
       const newInteraction = interactions[newsArticleId] == null
-      if (newInteraction) {
-        addInteractionAPI({ newsArticleId, clicked: false })
-      }
+      if (!newInteraction) return
+
+      // Store new interaction in backend
+      addInteractionAPI({ newsArticleId, clicked: false })
 
       // Store interaction in redux store
-      dispatch(
+      store.dispatch(
         addInteraction({
           newsArticleId: newsArticleId,
           clicked: false,
@@ -43,8 +37,7 @@ function NewsFeed() {
 
   async function fetchNews() {
     const news = await getNews({ limit: 5, offset: 0 })
-    // TODO: filter in Backend
-    return news.items.filter((newsItem) => newsItem.image)
+    return news.items
   }
 
   async function loadAdditionalNewsItems() {
